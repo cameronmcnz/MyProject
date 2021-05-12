83abb96126d6c1aa07403fe47d4110543d0a381a
Joey Shabidoo <me@mcnz.com>
2021-05-12 10:50:07 -0700

    TestGreeting and 3 test cases added

---
+++ src/test/java/hello/TestGreeting.java
@@ -0,0 +1,25 @@
+package hello;
+
+import org.junit.*;
+import hello.Greeter;
+
+public class TestGreeting {
+	
+	@Test
+	public void testGreeter() {
+		Greeter msg = new Greeter();
+		Assert.assertEquals("Hello world!", msg.sayHello());
+	}
+	
+	@Test
+	public void testSomething() {
+		Assert.assertTrue(true);
+	}
+	
+	@Test
+	public void testSomethingElse() {
+		Assert.assertNotEquals("maven", "gradle");
+	}
+	
+
+}
